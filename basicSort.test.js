fb020b66b69ab3762df065d1eb828adc67648606
Guy <gkamin6@gmail.com>
2021-05-15 18:32:26 -0700

    update unit test

--- basicSort.test.js
+++ basicSort.test.js
@@ -1,8 +1,8 @@
 const basicSort = require('./basicSort')
 
 test('properly sorts an array', () => {
-    const numsUnsorted = [30, 10, 20, 60, 40, 50, 80, 90, 70, 100, 29832983, 28]
-    const numsSorted = [10, 20, 28, 30, 40, 50, 60, 70, 80, 90, 100, 29832983]
+    const numsUnsorted = [30, 10, 20, 60, 644, 40, 50, 80, 90, 70, 12]
+    const numsSorted = [10, 12, 20, 30, 40, 50, 60, 70, 80, 90, 644]
     const wordsUnsorted = ["Juliet", "Romeo", "Delta"]
     const wordsSorted = ["Delta", "Juliet", "Romeo"]
     expect(basicSort(numsUnsorted)).toEqual(numsSorted)
