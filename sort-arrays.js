8f7b030f13009519e48148a5e70d9fd708b61db8
Guy <gkamin6@gmail.com>
2021-05-16 20:29:20 -0700

    Explain where function comes from

--- sort-arrays.js
+++ sort-arrays.js
@@ -1,3 +1,4 @@
+// importing the selection sort from basicSort.js
 const sort = require('./basicSort')
 let nums = [18, 12, 72, 99, 4, 1]
 let words = ["Bob", "Charlie", "Alpha", "Zulu", "Yankee", "Xray"]
